c56bba85d84e583408dc9f78a926bddda2f4616b
lsh <pttc733@gmail.com>
2023-03-24 14:30:58 +0900

    ver1.2_반응형 레이아웃

--- js/common.js
+++ js/common.js
@@ -1,8 +1,50 @@
 const $ham = document.querySelector(".ham");
 const $hd = document.querySelector(".hd");
+const $gnbLi = document.querySelectorAll(".gnb>li");
+// const $subMenu = $gnbLi.querySelectorAll(".sub");
+
 $ham.addEventListener("click", () => {
   $hd.classList.toggle("on");
 });
+// $gnbLi.forEach((menu) => {
+//   menu.addEventListener("click", () => {
+//     $subMenu.classList.toggle("on");
+//   });
+// });
+
+// $gnbLi.forEach((menu) => {
+//   menu.addEventListener("click", () => {
+//     const subMenu = menu.querySelector(".sub");
+//     if (subMenu.style.display === "none") {
+//       subMenu.style.display = "block";
+//     } else {
+//       subMenu.style.display = "none";
+//     }
+//   });
+// });
+
+// if (window.innerWidth < 1151) {
+//   $gnbLi.forEach(($gnbLi) => {
+//     $gnbLi.addEventListener("click", () => {
+//       const subMenu = $gnbLi.querySelector(".sub");
+//       if (subMenu.style.display === "none" || subMenu.style.display === "") {
+//         subMenu.style.display = "block";
+//       } else {
+//         subMenu.style.display = "none";
+//       }
+//     });
+//   });
+// } else {
+//   $gnbLi.forEach(() => {
+//     const subMenu = $gnbLi.querySelector(".sub");
+//     $gnbLi.addEventListener("mouseover", () => {
+//       subMenu.style.display = "block";
+//     });
+//     $gnbLi.addEventListener("mouseout", () => {
+//       subMenu.style.display = "none";
+//     });
+//   });
+// }
 
 function loadItems() {
   return fetch(
